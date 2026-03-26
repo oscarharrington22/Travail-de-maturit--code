7fcba469c53b1771836b3f078557dd52ab7d04b2
oscarharrington22 <oscar.atrhur.harrington@gmail.com>
2026-03-26 16:37:48 +0100

    Utilisation du site d'administration django

--- django-web-app/merchex/listings/admin.py
+++ django-web-app/merchex/listings/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from listings.models import Band , Listings
 
-# Register your models here.
+class BandAdmin (admin.ModelAdmin) :
+    list_display = ('name', 'year_formed', 'genre')
+
+admin.site.register(Band, BandAdmin)
+
+class ListingsAdmin (admin.ModelAdmin) :
+    list_display = ('title', 'band')
+
+admin.site.register(Listings)
--- django-web-app/merchex/listings/models.py
+++ django-web-app/merchex/listings/models.py
@@ -1,4 +1,24 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Band(models.Model):
+    def __str__ (self):
+        return f'{self.name}'
+    
+    class Genre(models.TextChoices):
+        HIP_HOP = 'HH'
+        SYNTH_POP = 'SP'
+        ALTERNATIVE_ROCK = 'AR'
+
     name = models.fields.CharField(max_length=100)
+    biography = models.fields.CharField(max_length=1000)
+    year_formed = models.fields.IntegerField(
+    validators=[MinValueValidator(1900), MaxValueValidator(2021)]
+    )
+    active = models.fields.BooleanField(default=True)
+    official_homepage = models.fields.URLField(null=True, blank=True)
+    genre = models.fields.CharField(choices=Genre.choices, max_length=5)
+
+class Listings (models.Model) :
+    title = models.fields.CharField(max_length=100)
+    band = models.ForeignKey(Band, null=True, on_delete=models.SET_NULL)
