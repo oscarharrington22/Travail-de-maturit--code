492068b466b9e3ee73502c07ad7415120442da28
oscarharrington22 <oscar.atrhur.harrington@gmail.com>
2026-05-13 15:47:50 +0200

    Passage d'une focntion login à une classe login

--- fotoblog/fotoblog/authentication/views.py
+++ fotoblog/fotoblog/authentication/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render, redirect
+from django.views.generic import View
 
 from django.contrib.auth import login, authenticate, logout
 
 from . import forms
 
-def login_page(request):
-    form = forms.LoginForm()
-    message=''
-    if request.method == 'POST':
-        form = forms.LoginForm(request.POST)
+class LoginPageView(View):
+    template_name = 'authentication/login.html'
+    form_class = forms.LoginForm
+
+    def get(self, request):
+        form = self.form_class()
+        message = ''
+        return render(request, self.template_name, context={'form': form, 'message': message})
+        
+    def post(self, request):
+        form = self.form_class(request.POST)
         if form.is_valid():
             user = authenticate(
                 username=form.cleaned_data['username'],
@@ -17,9 +24,8 @@ def login_page(request):
             if user is not None:
                 login(request, user)
                 return redirect('home')
-            
-            message = 'Identifiants invalides.'
-    return render(request, 'authentication/login.html', context={'form': form, 'message': message})
+        message = 'Identifiants invalides.'
+        return render(request, self.template_name, context={'form': form, 'message': message})
 
 def logout_user(request):
     logout(request)
