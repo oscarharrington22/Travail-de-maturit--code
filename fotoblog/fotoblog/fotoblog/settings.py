5e5d20c311489418c513b870f80564917734c501
oscarharrington22 <oscar.atrhur.harrington@gmail.com>
2026-05-22 16:07:39 +0200

    Création du logout dans l'url

--- fotoblog/fotoblog/fotoblog/settings.py
+++ fotoblog/fotoblog/fotoblog/settings.py
@@ -127,3 +127,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'authentication.User'
 
 LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'home'
--- fotoblog/fotoblog/fotoblog/urls.py
+++ fotoblog/fotoblog/fotoblog/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 import authentication.views
@@ -22,7 +23,9 @@ import blog.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', authentication.views.LoginPageView.as_view(), name='login'),
-    path('logout/', authentication.views.logout_user, name='logout'),
+    path('', LoginView.as_view(template_name='authentication/login.html',
+            redirect_authenticated_user=True), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    #Path pour le logout ne fonctionne pas
     path('home/', blog.views.home, name='home'),
 ]
