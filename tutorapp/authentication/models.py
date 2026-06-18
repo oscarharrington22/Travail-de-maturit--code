a390983e931a1d57efceb179a1f4a8667c49efe3
oscarharrington22 <oscar.atrhur.harrington@gmail.com>
2026-06-18 12:25:13 +0200

    Création de la classe User personalisée

--- tutorapp/authentication/models.py
+++ tutorapp/authentication/models.py
@@ -1,3 +1,60 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+class User (AbstractUser):
+    SUJETS_CHOIX=[
+        ('francais', 'Français'),
+        ('anglais', 'Anglais'),
+        ('allemand', 'Allemand'),
+        ('maths', 'Mathématiques'),
+        ('physique', 'Physique'),
+        ('éco', 'Économie'),
+        ('chimie', 'Chimie'),
+        ('bio', 'Biologie'),
+        ('philo', 'Philosophie'),
+        ('psyco', 'Psycologie'),
+        ('musique', 'Musique'),
+        ('art', 'Arts visuels')
+    ]
+
+    profile_photo = models.ImageField(
+        verbose_name='Photo de profil',
+        null=True, 
+        blank=True
+        )
+    age = models.PositiveIntegerField(
+        default=10, 
+        validators=[MinValueValidator(1), MaxValueValidator(100)], 
+        verbose_name='Âge'
+        )
+    ville = models.CharField(
+        max_length=100, 
+        verbose_name="Lieu d'habitation"
+        )
+
+    possible_prof = models.BooleanField(
+        default=False, 
+        verbose_name='Peu donner des cours'
+        )
+    sujets_prof = models.CharField(
+        choices=SUJETS_CHOIX,
+        verbose_name='Matières', 
+        blank=True
+        )
+    tarif = models.PositiveIntegerField(
+        validators=[MinValueValidator(0)], 
+        verbose_name="Tariff à l'heure" ,
+        blank=True,
+        null=True
+        )
+    
+    possible_eleve = models.BooleanField(
+        default=False,
+        verbose_name='À besoin de cours'
+    )
+    sujets_eleve = models.CharField(
+       choices=SUJETS_CHOIX,
+       verbose_name='Matières', 
+       blank=True 
+    )
