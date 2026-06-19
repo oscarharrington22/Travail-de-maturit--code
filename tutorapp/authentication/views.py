f11c5bfcdc6bae1982746d3272bd88428f0df131
oscarharrington22 <oscar.atrhur.harrington@gmail.com>
2026-06-19 11:41:10 +0200

    Création des gabarits de base et de login, de la vue ansi que du chemin url et du formulaire de connection

--- tutorapp/authentication/views.py
+++ tutorapp/authentication/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.contrib.auth import login, authenticate
 
-# Create your views here.
+from . import forms
+
+def login_page(request):
+    form = forms.LoginForm()
+    message =''
+    if request.method == 'POST':
+        form = forms.LoginForm(request.POST)
+        if form.is_valid():
+            user = authenticate(
+                username=form.cleaned_data['username'],
+                password=form.cleaned_data['password'],
+            )
+            if user is not None:
+                login(request, user)
+                message = f'Conection réussie en tant que {user.username}'
+            else:
+                message = 'Identifiants invalides.'
+    return render(request, 'authentication/login.html', context={'form': form, 'message': message})
